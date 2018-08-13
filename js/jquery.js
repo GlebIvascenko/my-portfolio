3629dc9eb3c4340f3bb71b9cdf8a777acc0c1793
Gleb <ivascenko.gleb@gmail.com>
2018-08-13 09:06:49 +0300

    Updates

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+//add smoth scrolling when clickn on anchor tag in nav menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
